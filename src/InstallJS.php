6cc92fc86fc8c1fbf3a8e37603def35a9a875bf5
John Saunders <john@yettimedia.co.uk>
2017-02-27 10:56:40 +0000

    Install js

---
+++ src/InstallJS.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace JellyMould;
+
+
+class InstallJS
+{
+    /**
+     * @var array
+     */
+    protected $installFilesArray = array('jellymouldfrontend.js', 'jellymouldpage.js', 'spectrum.js');
+
+    /**
+     * InstallJS constructor.
+     * @param null $jellymouldFrontendJSDirectory
+     */
+    public function __construct($jellymouldFrontendJSDirectory = null)
+    {
+        $jellymouldFrontendJSDirectory = isset($jellymouldFrontendJSDirectory) ? rtrim($jellymouldFrontendJSDirectory, '/') . '/' : null;
+        $this->installFiles($jellymouldFrontendJSDirectory);
+
+    }
+
+    protected function installFiles($jellymouldFrontendJSDirectory = null)
+    {
+        foreach ($this->installFilesArray as $file) {
+            $jSpath = __DIR__ . '/../js/' . $file;
+            $srcExists = file_exists($jSpath);
+            if (isset($jellymouldFrontendJSDirectory) && $srcExists) {
+                $writePath = $jellymouldFrontendJSDirectory . $file;
+                $jSData = file_get_contents($jSpath);
+                file_put_contents($writePath, $jSData);
+            }
+        }
+    }
+
+}
